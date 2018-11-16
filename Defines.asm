e30a6a6468d613b22380330e5469b7c94e437f21
RussianManSMWC <44837809+RussianManSMWC@users.noreply.github.com>
2018-11-16 22:11:21 +0600

    Frame counter and $0500
    $2F is game's frame counter, while $0500 is... uh... a weird thing.

--- Defines.asm
+++ Defines.asm
@@ -5,6 +5,8 @@
 
 InterruptedFlag = $23		    ;Used to determine if game got interrupted with Non-maskable interrupt. one routine specifically waits for NMI and will end waiting after it happens
 
+FrameCounter = $2F          ;Self-explanatory, increments every frame.
+
 Show2ndPlayerScore = $39	  ;flag for player 2 score to display
 
 GameAorBFlag = $3A          ;self explanatory, set if player chose game B
@@ -33,13 +35,13 @@ PowHitsLeft = $70
 
 ScreenShakeTimer = $71
 
-HighScore = $91				;3 bytes
+HighScore = $91				      ;3 bytes
 
-Player1Score = $95			;3 bytes
+Player1Score = $95			    ;3 bytes
 
-Player1Score = $99			;3 bytes
+Player1Score = $99			    ;3 bytes
 
-Player2ScoreDisplay = $9E	;seems to be another flag for score display for player 2, except this doesn't handles "II" tile on screen.
+Player2ScoreDisplay = $9E	  ;seems to be another flag for score display for player 2, except this doesn't handles "II" tile on screen.
 
 BonusTimeSecs = $04B1
 
@@ -48,3 +50,7 @@ BonusTimeMilliSecs = $04B2
 Player1BonusCoins = $04B5
 
 Player2BonusCoins = $04B6
+
+SoundPitch = $0500          ;or whatever, related with Freezie's explosion sound (when it makes platform icy), fireball sounds and somehow with red fireball's movement.
+
+
