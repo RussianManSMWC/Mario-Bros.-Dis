655754f8681a08a5858eba781fd852e1cd961641
RussianManSMWC <44837809+RussianManSMWC@users.noreply.github.com>
2019-03-22 17:29:52 +0600

    $18-1B and $09
    $18-1B contain controllers inputs, and $09 contains active $2000 bits

--- Defines.asm
+++ Defines.asm
@@ -3,38 +3,48 @@
 
 ;RAM Adresses
 
-Reg2001BitStorage = $0A                 ;contains bits to be enabled/disabled for register $2001
+Reg2000BitStorage = $09			;contains bits to be enabled/disabled for register $2000
+Reg2001BitStorage = $0A			;contains bits to be enabled/disabled for register $2001
 
-InterruptedFlag = $23                   ;Used to determine if game got interrupted with Non-maskable interrupt. one routine specifically waits for NMI and will end waiting after it happens
+;Controller addresses. Format: ABetUDLR, A = A button, B = B button, e = select, t = start, U = Up, D = Down, L = Left, R = Right.
+;Do note that InputPress only resets A and B bits after press.
 
-TimingTimer = $2A                       ;timer used to decrease other timers.
+Controller1InputHolding = $18		;controller input bits for player 1, holding.
+Controller1InputPress = $19		;controller input bits for player 1, press.
 
-GeneralTimer2B = $2B	                  ;used as timer for various things, for example as timer for bonus end, for each coin display, multiplication display and perfect/no bonus message and for unpause
-ShakeTimer = $2C	                      ;timer used for screen shaking when POW block is hit
-TransitionTimer = $2D	                  ;used mostly for transitions, specifically, how long it takes before starting new phase after last enemy falls offscreen, timer before transitioning to bonus end and from it.
-PipeDelayTimer = $2E	                  ;used for sprites coming out of pipes. when timer's zero, sprite comes out of pipe.
+Controller2InputHolding = $1A		;controller input bits for player 2, holding.
+Controller2InputPress = $1B		;controller input bits for player 2, press.
 
-FrameCounter = $2F			                ;Self-explanatory, increments every frame.
+InterruptedFlag = $23			;Used to determine if game got interrupted with Non-maskable interrupt. one routine specifically waits for NMI and will end waiting after it happens
 
-DemoFlag = $30				                  ;flag set when demo plays
+TimingTimer = $2A			;timer used to decrease other timers.
 
-Show2ndPlayerScore = $39	              ;flag for player 2 score to display
+GeneralTimer2B = $2B			;used as timer for various things, for example as timer for bonus end, for each coin display, multiplication display and perfect/no bonus message and for unpause
+ShakeTimer = $2C			;timer used for screen shaking when POW block is hit
+TransitionTimer = $2D			;used mostly for transitions, specifically, how long it takes before starting new phase after last enemy falls offscreen, timer before transitioning to bonus end and from it.
+PipeDelayTimer = $2E			;used for sprites coming out of pipes. when timer's zero, sprite comes out of pipe.
 
-GameAorBFlag = $3A                      ;self explanatory, set if player chose game B
+FrameCounter = $2F			;Self-explanatory, increments every frame.
 
-PaletteFlag = $3F                       ;If 1, use palette for normal gameplay, otherwise use title screen palette.
+DemoFlag = $30				;flag set when demo plays
 
-GameplayMode = $40                      ;used for pointers to handle various gameplay aspects, such as (un)pausing, proceeding to next phase, coin counting after "Test Your Skill!" and other.
+Show2ndPlayerScore = $39		;flag for player 2 score to display
 
-DisplayLevelNum = $41		                ;
+GameAorBFlag = $3A			;self explanatory, set if player chose game B
 
-FreezeFlag = $42                        ;POSSIBLY freeze flag, though it doesn't affects player gravity (only collision) and green fireballs. only applied in normal gameplay or demo.
+PaletteFlag = $3F			;If 1, use palette for normal gameplay, otherwise use title screen palette.
+
+GameplayMode = $40			;used for pointers to handle various gameplay aspects, such as (un)pausing, proceeding to next phase, coin counting after "Test Your Skill!" and other.
+
+DisplayLevelNum = $41			;
+
+FreezeFlag = $42			;POSSIBLY freeze flag, though it doesn't affects player gravity (only collision) and green fireballs. only applied in normal gameplay or demo.
 
 Player1Lives = $48
 
 MarioGameOverFlag = $49
 
-GameOverFlag = $4A                      ;non-player specific game over flag.
+GameOverFlag = $4A			;non-player specific game over flag.
 
 Player2Lives = $4C
 
@@ -44,15 +54,15 @@ Player2DisplayFlag = $4E
 
 PowHitsLeft = $70
 
-POWPowerTimer = $71                     ;timer set when POW is hit, to run hit interaction with everything on-screen. 
+POWPowerTimer = $71			;timer set when POW is hit, to run hit interaction with everything on-screen. 
 
-HighScore = $91				                  ;3 bytes
+HighScore = $91				;3 bytes
 
-Player1Score = $95			                ;3 bytes
+Player1Score = $95			;3 bytes
 
-Player1Score = $99			                ;3 bytes
+Player1Score = $99			;3 bytes
 
-Player2ScoreDisplay = $9E               ;seems to be another flag for score display for player 2, except this doesn't handles "II" tile on screen.
+Player2ScoreDisplay = $9E		;seems to be another flag for score display for player 2, except this doesn't handles "II" tile on screen.
 
 BonusTimeSecs = $04B1
 
@@ -62,6 +72,6 @@ Player1BonusCoins = $04B5
 
 Player2BonusCoins = $04B6
 
-RandomNumberStorage = $0500             ;
+RandomNumberStorage = $0500		;
 
 
